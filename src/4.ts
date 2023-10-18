70ae2dda01e1289c359c89f8aa88840f295d8bb5
VasOleksii <vasilenkooleksii@gmail.com>
2023-10-18 12:00:58 +0300

    Done
    OOP + TS

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,69 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-house.openDoor(person.getKey());
+class Person {
+  constructor(public name: string, private key: Key) {}
+  getKey() {
+    return this.key;
+  }
+}
 
-house.comeIn(person);
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
 
+  constructor(key: Key) {
+    this.key = key;
+  }
 
-export {};
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`"${person.name} Вхід дозволено."`);
+    } else {
+      console.log(`"Двері закриті. ${person.name} Вхід заборонено."`);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Двері відчинено.');
+    } else {
+      console.log(`"Неправильний ключ. Двері залишаються закритими."`);
+    }
+  }
+}
+
+const BobKey = new Key();
+const JhonKey = new Key();
+console.log(BobKey);
+console.log(JhonKey);
+
+const Bob = new Person('Bob', BobKey);
+console.log(Bob);
+
+const Jhon = new Person('Jhon', JhonKey);
+console.log(Jhon);
+
+const BobHouse = new MyHouse(BobKey);
+console.log(BobHouse);
+
+BobHouse.openDoor(Jhon.getKey());
+BobHouse.comeIn(Jhon);
+
+BobHouse.openDoor(Bob.getKey());
+BobHouse.comeIn(Bob);
+BobHouse.comeIn(Jhon);
